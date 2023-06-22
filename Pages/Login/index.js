8b74e3ab97fdb93d095e2dc04bc987b6a950e232
thisiskarthisk <karthikeyan2771999@gmail.com>
2023-06-22 12:05:34 +0530

    Backgroung colour change

--- Pages/Login/index.js
+++ Pages/Login/index.js
@@ -13,7 +13,7 @@ export default function LOGIN() {
     const styles = StyleSheet.create({
         container: {
           flex: 1,
-          backgroundColor: "#c99dd1",
+          backgroundColor: "#ffffff",
           alignItems: "center",
           justifyContent: "center",
         },
