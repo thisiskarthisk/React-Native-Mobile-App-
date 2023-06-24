6e20cc722bf6fe2887b7ae54834941039f329ea1
thisiskarthisk <karthikeyan2771999@gmail.com>
2023-06-24 17:31:05 +0530

    error

--- App.js
+++ App.js
@@ -16,13 +16,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LOGIN from './src/views/Pages/Login';
-import { Navigation } from './src/views/components/Navigation'; // Updated import statement
+// import { Navigation } from './src/views/components/Navigation'; // Updated import statement
 
 export default function App() {
   return (
     <>
       <StatusBar/>
-      <Navigation/> {/* Updated component usage */}
+      {/* <Navigation/> Updated component usage */}
       <LOGIN/>
     </>
   );
